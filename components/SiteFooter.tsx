a6e47f96f97cb7910761fea76a36bddfab0e9c4c
qd <quangquya2@gmail.com>
2026-03-25 10:12:08 +0700

    feat: add SiteFooter component including brand information, contact details, and an embedded map.

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -41,7 +41,7 @@ export function SiteFooter() {
                 </a>
               </li>
               <li>
-                <div className="rounded-xl overflow-hidden w-full h-44 border border-emerald-400/30 shadow-lg">
+                <div className="overflow-hidden w-full h-44 border border-emerald-400/30 shadow-lg">
                   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3869.537649379725!2d109.18716049999999!3d14.104448999999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x316f37d14647decb%3A0xc99b261b8447a7f8!2zQ-G7rWEgaMOgbmcgxJFp4buHbiBuxrDhu5tjIE1haSBWaW5o!5e0!3m2!1svi!2s!4v1774407991432!5m2!1svi!2s" width="100%" height="100%" style={{ border: 0 }} allowFullScreen
                     loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"
